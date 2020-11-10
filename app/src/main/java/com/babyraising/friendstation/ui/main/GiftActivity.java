816aeb02d3e6155b4ac2ec19c99f68803728ddd4
QianDashuai <491787891@qq.com>
2020-11-10 16:38:22 +0800

    [add]增加送礼物页面

---
+++ app/src/main/java/com/babyraising/friendstation/ui/main/GiftActivity.java
@@ -0,0 +1,45 @@
+package com.babyraising.friendstation.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.babyraising.friendstation.R;
+import com.babyraising.friendstation.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+
+
+@ContentView(R.layout.activity_gift)
+public class GiftActivity extends BaseActivity {
+
+    @Event(R.id.back)
+    private void back(View view) {
+        finish();
+    }
+
+    @Event(R.id.layout_recharge)
+    private void layoutRechargeClick(View view) {
+
+    }
+
+    @Event(R.id.send)
+    private void sendClick(View view) {
+
+    }
+
+    @ViewInject(R.id.count)
+    private TextView count;
+
+    @ViewInject(R.id.list)
+    private RecyclerView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
