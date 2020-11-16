79e61c4021f168aeeb3d140abcabfad16280e308
QianDashuai <491787891@qq.com>
2020-11-16 17:44:37 +0800

    [add]增加我的收益页面

---
+++ app/src/main/java/com/babyraising/friendstation/ui/main/MyEarningActivity.java
@@ -0,0 +1,38 @@
+package com.babyraising.friendstation.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.babyraising.friendstation.R;
+import com.babyraising.friendstation.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_my_earning)
+public class MyEarningActivity extends BaseActivity {
+
+    @Event(R.id.back)
+    private void backClick(View view) {
+        finish();
+    }
+
+    @Event(R.id.charge)
+    private void chargeClick(View view) {
+
+    }
+
+    @ViewInject(R.id.drawal)
+    private TextView drawal;
+
+    @ViewInject(R.id.balance)
+    private TextView balance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
