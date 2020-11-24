4447575e2dd8efd2e521efa1074ba728e4b66bf4
QianDashuai <491787891@qq.com>
2020-11-24 09:46:17 +0800

    [add]增加部分图片

---
+++ app/src/main/java/com/babyraising/friendstation/ui/main/VoiceTipActivity.java
@@ -0,0 +1,18 @@
+package com.babyraising.friendstation.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.friendstation.R;
+import com.babyraising.friendstation.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_voice_tip)
+public class VoiceTipActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
