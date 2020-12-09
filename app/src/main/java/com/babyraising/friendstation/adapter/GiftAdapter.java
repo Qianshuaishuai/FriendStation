06f863f90c84f2063fe2a2e739b51d0fe39c7f48
QianDashuai <491787891@qq.com>
2020-12-09 11:52:11 +0800

    [add]增加礼物界面

---
+++ app/src/main/java/com/babyraising/friendstation/adapter/GiftAdapter.java
@@ -0,0 +1,65 @@
+package com.babyraising.friendstation.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.babyraising.friendstation.R;
+import com.babyraising.friendstation.bean.TaskDetailBean;
+
+import org.xutils.x;
+
+import java.util.List;
+
+public class GiftAdapter extends RecyclerView.Adapter<GiftAdapter.ViewHolder> {
+
+    private List<String> mList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView  nameTxt, countTxt;
+        ImageView iconIv;
+
+        public ViewHolder(View view) {
+            super(view);
+            nameTxt = (TextView) view.findViewById(R.id.name);
+            countTxt = (TextView) view.findViewById(R.id.count);
+            iconIv = (ImageView) view.findViewById(R.id.icon);
+        }
+
+    }
+
+    public GiftAdapter(List<String> mList) {
+        this.mList = mList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_gift, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onClick(int position);
+    }
+
+    private OnItemClickListener listener;
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+}
--- app/src/main/java/com/babyraising/friendstation/ui/main/GiftActivity.java
+++ app/src/main/java/com/babyraising/friendstation/ui/main/GiftActivity.java
@@ -3,20 +3,35 @@ package com.babyraising.friendstation.ui.main;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.babyraising.friendstation.FriendStationApplication;
 import com.babyraising.friendstation.R;
+import com.babyraising.friendstation.adapter.GiftAdapter;
 import com.babyraising.friendstation.base.BaseActivity;
+import com.babyraising.friendstation.bean.UserAllInfoBean;
+import com.babyraising.friendstation.decoration.SpaceItemDecoration;
+import com.babyraising.friendstation.util.DisplayUtils;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @ContentView(R.layout.activity_gift)
 public class GiftActivity extends BaseActivity {
+
+    private UserAllInfoBean userInfoBean;
+    private GiftAdapter adapter;
 
     @Event(R.id.back)
     private void back(View view) {
@@ -38,10 +53,62 @@ public class GiftActivity extends BaseActivity {
     private TextView count;
 
     @ViewInject(R.id.list)
-    private RecyclerView list;
+    private RecyclerView recycleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        userInfoBean = ((FriendStationApplication) getApplication()).getUserAllInfo();
+        count.setText("" + userInfoBean.getUserCount().getNumCoin());
+    }
+
+    private void initView() {
+        List<String> testList = new ArrayList<>();
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        testList.add("1");
+        adapter = new GiftAdapter(testList);
+        GridLayoutManager manager = new GridLayoutManager(this, 4);
+        recycleList.setAdapter(adapter);
+        recycleList.setLayoutManager(manager);
+
+        WindowManager wm1 = this.getWindowManager();
+        int width1 = wm1.getDefaultDisplay().getWidth() - DisplayUtils.dp2px(this, 30);
+        int itemWidth = DisplayUtils.dp2px(this, 73); //每个item的宽度
+        recycleList.addItemDecoration(new SpaceItemDecoration((width1 - itemWidth * 4) / 8));
     }
 }
